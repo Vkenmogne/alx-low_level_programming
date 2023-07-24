c944828be88319ede09dddb1288a78c4cf143dff
Vkenmogne <precious.vanelle@gmail.com>
2023-07-24 11:51:36 -0700

    betty style fixed

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,18 @@
 #include "main.h"
 /**
- *swap_int is a function that swap the values of two integers
- *@a: interger to swap
- *@b: integer to swap
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
-
-void swap_int(int *a, int *b)
+int _strlen(char *s)
 {
-	int m;
+	int longi = 0;
 
-	m = *a;
-	*a = *b;
-	*b = m;
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
 }
